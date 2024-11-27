e8f0603d02d36a2bf7847f8e82abfbbcd18f1e8e
Alex5727 <alejandrogrjalva@gmail.com>
2024-11-27 10:10:45 -0700

    hola

---
+++ CarrilloShop2/AppDBcontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarrilloShop2
+{
+    public class AppDBcontext: DbContext
+    {
+        public AppDBcontext(DbContextOptions<AppDBcontext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ CarrilloShop2/Services/ClienteService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarrilloShop2.Services
+{
+    public class ClienteService
+    {
+        private AppDBcontext _context;
+
+        public ClienteService(AppDBcontext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
